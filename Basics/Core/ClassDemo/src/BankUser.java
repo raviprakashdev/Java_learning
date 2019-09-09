f89ee4075e2200d0a1c6c7a612c5d2fb16c1bdee
Ravi Prakash <raviprakash01a@gmail.com>
2019-09-10 01:02:47 +0530

    learnt to call constructor

--- Basics/Core/ClassDemo/src/BankUser.java
+++ Basics/Core/ClassDemo/src/BankUser.java
@@ -8,6 +8,12 @@ public class BankUser {
         john.accAmount(10000);
 
         john.getInfo();
+        BankApp ravi = new BankApp("Sun light");
+
+        ravi.accAge(25);
+        ravi.accAmount(30000);
+
+        ravi.getInfo();
 
     }
 }
