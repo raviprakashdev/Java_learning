184d88470da6806e5333f5ef48a01707eefe05b5
Ravi Prakash <raviprakash01a@gmail.com>
2019-09-26 23:49:52 +0530

    pattern

--- university/Progs/PatternA/src/PatternA.java
+++ university/Progs/PatternA/src/PatternA.java
@@ -2,11 +2,30 @@ public class PatternA {
 
     public static void main(String[] args)
     {
-        for (int i=1;i<=5;i++){
-            for (int j=1;j<=i;j++){
-                System.out.print(j);
+
+//        Patterna A
+//        for (int i=1;i<=5;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println(" ");
+//        }
+
+//        Pattern B
+
+//        for(int i=5; i>0; i--){
+//            for (int j=0;j<i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+ //        Pattern B
+
+        for(int i=5; i>0; i--){
+            for (int j=0;j<i;j++){
+                System.out.print(j+1);
             }
-            System.out.println(" ");
+            System.out.println("");
         }
-    }
+        }
 }
